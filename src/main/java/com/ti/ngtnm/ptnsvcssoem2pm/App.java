0d1eec9d2a951859a405cdfe28f36d4b88bbee1e
Alessandro <alessandro.giangreco@gmail.com>
2018-12-11 16:51:07 +0100

    Add support for spring framework

--- src/main/java/com/ti/ngtnm/ptnsvcssoem2pm/App.java
+++ src/main/java/com/ti/ngtnm/ptnsvcssoem2pm/App.java
@@ -4,19 +4,33 @@
  * and open the template in the editor.
  */
 package com.ti.ngtnm.ptnsvcssoem2pm;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
 
 /**
  *
  * @author Alex
  */
-public class app {
+@ComponentScan(basePackages = "com.ti")
+@Configuration
+public class App {
 
   final static Logger logger
-    = LoggerFactory.getLogger(app.class);
+    = LoggerFactory.getLogger(App.class);
 
   public static void main(String[] args) {
+
+    logger.debug("Initializing spring framework");
+
+    AnnotationConfigApplicationContext ctx
+      = new AnnotationConfigApplicationContext(App.class);
+
+    App app = ctx.getBean(App.class);
 
     logger.info("-- Starting PTN Conversion Tool --");
     // Prints "Hello, World" to the terminal window.
