e9db09f7170f2cdba447926fc3204b24200bd863
Alessandro <alessandro.giangreco@gmail.com>
2018-12-11 16:07:06 +0100

    Aggiunta funzionalità di logging

--- src/main/java/com/ti/ngtnm/ptnsvcssoem2pm/app.java
+++ src/main/java/com/ti/ngtnm/ptnsvcssoem2pm/app.java
@@ -4,14 +4,22 @@
  * and open the template in the editor.
  */
 package com.ti.ngtnm.ptnsvcssoem2pm;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  *
  * @author Alex
  */
 public class app {
-    public static void main(String[] args) {
-        // Prints "Hello, World" to the terminal window.
-        System.out.println("Hello, World");
-    }
+
+  final static Logger logger
+    = LoggerFactory.getLogger(app.class);
+
+  public static void main(String[] args) {
+
+    logger.info("-- Starting PTN Conversion Tool --");
+    // Prints "Hello, World" to the terminal window.
+    System.out.println("Hello, World");
+  }
 }
