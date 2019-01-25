2ee9d6895cafb47ef6a2f4d907f9dbe8d9bd8967
Alessandro <alessandro.giangreco@gmail.com>
2019-01-25 16:30:24 +0100

    renamed class source to identity

--- src/main/java/com/ti/ngtnm/ptnsvcssoem2pm/Entity.java
+++ src/main/java/com/ti/ngtnm/ptnsvcssoem2pm/Entity.java
@@ -1,4 +1,6 @@
 /*
+
+import javax.swing.text.html.parser.Entity;
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
@@ -6,6 +8,6 @@
 
 package com.ti.ngtnm.ptnsvcssoem2pm;
 
-class Source {
+class Entity {
 
 } // class Source
