93d337a549903438394c1803cc8df72d0dba65cb
Alessandro <alessandro.giangreco@gmail.com>
2019-01-25 16:41:11 +0100

    create entity class

--- src/main/java/com/ti/ngtnm/ptnsvcssoem2pm/Entity.java
+++ src/main/java/com/ti/ngtnm/ptnsvcssoem2pm/Entity.java
@@ -1,13 +1,39 @@
 /*
-
-import javax.swing.text.html.parser.Entity;
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
 
 package com.ti.ngtnm.ptnsvcssoem2pm;
+import java.util.HashMap;
 
 class Entity {
 
-} // class Source
+  public Entity () {
+    counters = new HashMap<String, String>();
+  } // public method
+
+  // counter Getter
+  public String getCounter(String counterkey) {
+    return this.counters.get(counterkey);
+  }
+
+  // counter Setter
+  public void setCounter(String counterkey, String countervalue) {
+    this.counters.put(counterkey,countervalue);
+  }
+
+  // failure Getter
+  public String getFailure() {
+    return this.failure;
+  }
+
+  // failure Setter
+  public void setFailure(String failure) {
+    this.failure = failure;
+  }
+
+  private HashMap <String,String> counters = null;
+  private String failure;
+
+} // class Entity
